0cde939583dc8fb6380709a82ccab1afc97bec53
jb corbin <101614021+jbc2313@users.noreply.github.com>
2022-10-13 19:28:46 -0500

    register form is working

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -3,38 +3,34 @@ import { useRouter } from "next/router";
 import { useForm } from 'react-hook-form'
 import { CreateUserInput } from "../schema/user.schema";
 import { trpc } from '../util/trpc'
-function RegisterPage() {
+function LoginPage() {
     const router = useRouter()
     const {handleSubmit, register} = useForm<CreateUserInput>()
 
-    const { mutate, error } = trpc.useMutation(['users.register-user'], {
-        onError: (error) => {
-        
-        },
-        onSuccess: () => {
-            router.push('/login')
-        }
-    })
+    // const { mutate, error } = trpc.useMutation(['users.register-user'], {
+    //     onError: (error) => {
+    //    
+    //     },
+    //     onSuccess: () => {
+    //         router.push('/login')
+    //     }
+    // })
 
     function onSubmit(values: CreateUserInput) {
-        mutate(values)
+        // mutate(values)
 
     }
 
     return<>
         <form onSubmit={handleSubmit(onSubmit)}>
-        {error && error.message}
-        <h1>Register</h1>
+        {/* error && error.message */}
+        <h1>Login</h1>
 
         <input type="email" placeholder="pam@dunder.net" {...register('email')} />
-        <br/>
-        <input type="text" placeholder="Andy" {...register('name')} />
-        <button type='submit'>Register</button>
-
-
+       
         </form>
-        <Link href="/login">Login</Link>
+        <Link href="/register">Register</Link>
     </>
 }
 
-export default RegisterPage;
+export default LoginPage;
